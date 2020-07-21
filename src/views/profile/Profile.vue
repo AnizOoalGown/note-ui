09a95471c308061d32a98a46c227e81fa3bf03d7
Your Name <wxh@example.com>
2020-07-21 17:10:43 +0800

    Profile UI

--- src/views/profile/Profile.vue
+++ src/views/profile/Profile.vue
@@ -1,56 +1,58 @@
 <template>
-    <div class="profile-container scrollbar">
-        <div class="title">个人信息</div>
-        <el-divider/>
-        <el-form :model="userInfoForm"
-                 status-icon
-                 :rules="userInfoRules"
-                 ref="userInfoForm"
-                 hide-required-asterisk
-                 label-width="100px">
-            <el-form-item label="用户名" prop="username">
-                <el-input v-model="userInfoForm.username" autocomplete="off"
-                          @input="saveUserInfoShow = true"/>
-            </el-form-item>
-            <el-form-item v-if="saveUserInfoShow">
-                <el-button type="primary" @click="submitUserInfoForm">保存</el-button>
-                <el-button @click="resetUserInfoForm">取消</el-button>
-            </el-form-item>
-        </el-form>
+    <div class="scrollbar">
+        <div class="profile-container">
+            <div class="title">个人信息</div>
+            <el-divider/>
+            <el-form :model="userInfoForm"
+                     status-icon
+                     :rules="userInfoRules"
+                     ref="userInfoForm"
+                     hide-required-asterisk
+                     label-width="100px">
+                <el-form-item label="用户名" prop="username">
+                    <el-input v-model="userInfoForm.username" autocomplete="off"
+                              @input="saveUserInfoShow = true"/>
+                </el-form-item>
+                <el-form-item v-if="saveUserInfoShow">
+                    <el-button type="primary" @click="submitUserInfoForm">保存</el-button>
+                    <el-button @click="resetUserInfoForm">取消</el-button>
+                </el-form-item>
+            </el-form>
 
-        <div class="title">修改密码</div>
-        <el-divider/>
-        <el-form :model="passwordForm"
-                 status-icon
-                 :rules="passwordRules"
-                 ref="passwordForm"
-                 hide-required-asterisk
-                 label-width="100px">
-            <el-form-item label="旧密码" prop="password">
-            <el-input type="password" v-model="passwordForm.password" autocomplete="off"
-                      @input="savePasswordShow = true"></el-input>
-            </el-form-item>
-            <el-form-item label="新密码" prop="newPassword">
-                <el-input type="password" v-model="passwordForm.newPassword" autocomplete="off"
-                          @input="savePasswordShow = true"></el-input>
-            </el-form-item>
-            <el-form-item label="确认密码" prop="newCheckPassword">
-                <el-input type="password" v-model="passwordForm.newCheckPassword" autocomplete="off"
-                          @input="savePasswordShow = true"></el-input>
-            </el-form-item>
-            <el-form-item v-if="savePasswordShow">
-                <el-button type="primary" @click="submitPasswordForm">保存</el-button>
-                <el-button @click="resetPasswordForm">取消</el-button>
-            </el-form-item>
-        </el-form>
+            <div class="title">修改密码</div>
+            <el-divider/>
+            <el-form :model="passwordForm"
+                     status-icon
+                     :rules="passwordRules"
+                     ref="passwordForm"
+                     hide-required-asterisk
+                     label-width="100px">
+                <el-form-item label="旧密码" prop="password">
+                    <el-input type="password" v-model="passwordForm.password" autocomplete="off"
+                              @input="savePasswordShow = true"></el-input>
+                </el-form-item>
+                <el-form-item label="新密码" prop="newPassword">
+                    <el-input type="password" v-model="passwordForm.newPassword" autocomplete="off"
+                              @input="savePasswordShow = true"></el-input>
+                </el-form-item>
+                <el-form-item label="确认密码" prop="newCheckPassword">
+                    <el-input type="password" v-model="passwordForm.newCheckPassword" autocomplete="off"
+                              @input="savePasswordShow = true"></el-input>
+                </el-form-item>
+                <el-form-item v-if="savePasswordShow">
+                    <el-button type="primary" @click="submitPasswordForm">保存</el-button>
+                    <el-button @click="resetPasswordForm">取消</el-button>
+                </el-form-item>
+            </el-form>
 
-        <div class="title">退出登录</div>
-        <el-divider/>
-        <el-button>退出登录</el-button>
+            <div class="title">退出登录</div>
+            <el-divider/>
+            <el-button>退出登录</el-button>
 
-        <div class="title">销户</div>
-        <el-divider/>
-        <el-button type="danger">销户</el-button>
+            <div class="title">销户</div>
+            <el-divider/>
+            <el-button type="danger">销户</el-button>
+        </div>
     </div>
 </template>
 
@@ -92,7 +94,8 @@
                 },
                 userInfoRules: {
                     username: [
-                        { required: true, message: '请输入用户名', trigger: 'blur' }
+                        { required: true, message: '请输入用户名', trigger: 'blur' },
+                        { max: 16, message: '长度小于16个字符', trigger: 'blur' }
                     ]
                 },
                 passwordRules: {
@@ -143,6 +146,10 @@
 </script>
 
 <style scoped>
+    .scrollbar {
+        width: 100%;
+    }
+
     .profile-container {
         width: 500px;
         margin: 0 auto;
